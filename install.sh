c01a07ae58ab6f7a11ecd804ebde18f1f1f0e7cf
Evgeny Sinelnikov <sin@altlinux.ru>
2017-06-02 06:02:26 +0300

    Complete fix of ALT#33427 problem again

--- install.sh
+++ install.sh
@@ -170,6 +170,7 @@ case "$(hostname -s)" in
 		apt-get install -y -qq task-auth-ad-sssd
 		apt-get clean
 		init_krb5_conf "$REALM"
+		ln -s /usr/lib64/ldb/modules/ldb /usr/lib64/samba/ldb
 		if [ -n "$host_nameserver" ]; then
 			disable_networkmanager_dns
 			disable_dhcpcd_resolvconf_hook
